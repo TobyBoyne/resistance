8eae017c606e17e172f52ab5fd011251413f7895
Toby Boyne <tobyboyne@gmail.com>
2020-03-28 16:56:18 +0000

    Added styling templates through HTML and CSS

--- game/views.py
+++ game/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from .models import GameInstance
+from django.utils import timezone
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
 
 def game_list(request):
-    return render(request, 'game/game_list.html', {})
+    games = GameInstance.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'game/game_list.html', {'games': games})
