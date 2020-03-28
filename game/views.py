f498f62eecbb9a509958c1efcda0cb1dcf4a8858
Toby Boyne <tobyboyne@gmail.com>
2020-03-28 20:04:56 +0000

    Created basic logic for Game in views

--- game/views.py
+++ game/views.py
@@ -3,6 +3,8 @@ from .models import GameInstance
 from .forms import NewGameForm
 from django.utils import timezone
 
+from game_logic.main import GameHandler
+
 
 def game_list(request):
     games = GameInstance.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -10,6 +12,8 @@ def game_list(request):
 
 def game_page(request, pk):
     game = get_object_or_404(GameInstance, pk=pk)
+    game_handler = GameHandler()
+    game.text = game_handler.play()
     return render(request, 'game/game_page.html', {'game':game})
 
 def new_game(request):
---
+++ game_logic/main.py
@@ -0,0 +1,10 @@
+class GameHandler:
+	def __init__(self):
+		pass
+
+	def play(self):
+		return "You win"
+
+if __name__ == "__main__":
+	g = GameHandler()
+	g.play()
